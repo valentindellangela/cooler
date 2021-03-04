ef7662e5cc1288c2ad68610f3c1da1191b79eff8
Valentin Dell'angela <valentin.dellangela@bigcommerce.com>
2021-03-04 14:52:19 +1100

    Test receiving webhooks

--- web/index.php
+++ web/index.php
@@ -1,5 +1,7 @@
 <?php
 
+use Symfony\Component\HttpFoundation\Request;
+
 require('../vendor/autoload.php');
 
 $app = new Silex\Application();
@@ -24,6 +26,47 @@ $app->get('/', function() use($app) {
 
 $app->get('/test', function() use($app) {
 	return "HI!";
+});
+
+$app->post('/webhooks', function(Request $request) use($app) {
+    $client = new GuzzleHttp\Client();
+    $client->request('POST', 'https://webhook.site/3126fcc2-fe6f-4518-bd00-8c5c8e09ad36', ['test']);
+
+    return;
+
+    // if (!$request->get('scope') === 'store/cart/converted') {
+    //     return;
+    // }
+    //
+    // $response = $client->request('POST', 'https://api-staging.cooler.dev/v1/footprint/products', [
+    //     GuzzleHttp\RequestOptions::JSON => [
+    //         'currency' => 'USD',
+    //         'items' => [
+    //             [
+    //                 'product_id' => 'B2-R',
+    //                 'quantity' => 1,
+    //                 'price' => 599
+    //             ]
+    //         ]
+    //     ]
+    // ]);
+    //
+    // if ($response->getStatusCode() === 200) {
+    //     $body = json_decode($response->getBody());
+    //     $neutralisationId = $body['id'];
+    //
+    //     $response = $client->request('POST', 'https://api-staging.cooler.dev/v1/footprint/products', [
+    //         GuzzleHttp\RequestOptions::JSON => [
+    //             'transactions' => [$neutralisationId]
+    //         ]
+    //     ]);
+    //
+    //     if ($response->getStatusCode() === 200) {
+    //         return 'success!';
+    //     }
+    // }
+    //
+    // return 'failed :(';
 });
 
 
